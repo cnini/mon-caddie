4152944793833f1612138705b298cd8951167139
cnini <hello.cynthia@outlook.fr>
2025-10-15 20:43:52 +0200

    ✨ f05 : ROUTE > add / route for user's home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router";
 import Login from "./components/pages/login/Login";
+import Home from "./components/pages/home/Home";
 import { AuthProvider } from "./context/AuthContext";
 // import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
     return (
         <AuthProvider>
             <Routes>
+                <Route index path="/" element={<Home />} />
                 <Route path="/auth" element={<Login />} />
             </Routes>
         </AuthProvider>
